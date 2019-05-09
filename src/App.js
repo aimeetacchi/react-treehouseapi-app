cd7c61e326ae252b70658ba1826e216484c748f2
Aimee <baines@admins-iMac-4.local>
2019-05-09 12:48:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h1>Hello World</h1>
+      <p>Starter files for creating your own react application, as Scss set up.</p>
+    </div>
+  );
+}
+
+export default App;
